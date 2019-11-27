597696d5cfd675f5ce24f2c35fe4a659b5c1e724
X-AROK <vanyanadeev@mail.ru>
2019-11-27 16:44:00 +0300

    Replaced Rectangle2D with Line2D

--- src/main/java/ru/xarok/musicvisualizer/visualizers/BarsVisualizer.java
+++ src/main/java/ru/xarok/musicvisualizer/visualizers/BarsVisualizer.java
@@ -3,6 +3,7 @@ package ru.xarok.musicvisualizer.visualizers;
 import ru.xarok.musicvisualizer.Visualizer;
 
 import java.awt.*;
+import java.awt.geom.Line2D;
 import java.awt.geom.Rectangle2D;
 
 public class BarsVisualizer extends Visualizer {
@@ -27,16 +28,20 @@ public class BarsVisualizer extends Visualizer {
 
         int height = getHeight();
         int width = getWidth();
-        float barWidth = (float)width / barsCount - gap;
-        Rectangle2D.Float rect2D = new Rectangle2D.Float();
+        float barWidth = (float)width / barsCount; // - gap;
+        //Rectangle2D.Float rect2D = new Rectangle2D.Float();
+        Line2D.Float line2D = new Line2D.Float();
+        g2.setStroke(new BasicStroke(barWidth - gap));
 
         for(int i = 0; i < barsCount; i++) {
             int samplePos = (int)Math.ceil((double)i/barsCount * samples.length);
-            float x = (barWidth + gap) * i;
+            float x = barWidth / 2 + barWidth * i;
             float y = height - Math.abs(samples[samplePos]) * height;
 
-            rect2D.setRect(x, y, barWidth, y);
-            g2.fill(rect2D);
+            line2D.setLine(x, height, x, y);
+            g2.draw(line2D);
+            //rect2D.setRect(x, y, barWidth, y);
+            //g2.fill(rect2D);
         }
     }
 }
